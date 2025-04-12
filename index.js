18ff5e87686a410202a3b078179e12e5867fd625
FelipeKoshino <58477693+FelipeKoshino@users.noreply.github.com>
2025-04-12 14:22:22 -0300

    Update index.js
    Add slider-animation for to sections

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 window.addEventListener('scroll', function() {
     const nav = document.getElementById('mainNav');
     if (window.scrollY > 50) { // Ajuste este valor conforme necessário (a distância para ativar a transparência)

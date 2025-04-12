410651f30daa14783dc0c1fe74a15b027a9e90f8
FelipeKoshino <58477693+FelipeKoshino@users.noreply.github.com>
2025-04-12 17:36:40 -0300

    Update index.js
    Add class sumir-m1 on .menu-1

--- index.js
+++ index.js
@@ -11,13 +11,16 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+const m1 = document.querySelector(".menu-1")
 
 window.addEventListener('scroll', function() {
     const nav = document.getElementById('mainNav');
     if (window.scrollY > 50) { // Ajuste este valor conforme necessário (a distância para ativar a transparência)
         nav.classList.add('nav-scrolled');
+        m1.classList.add('sumir-m1');
 
     } else {
         nav.classList.remove('nav-scrolled');
+        m1.classList.remove('sumir-m1');
     }
 });
